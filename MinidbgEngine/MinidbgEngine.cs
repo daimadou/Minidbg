d83caab900581fbf8099698c0617910fb3dc863f
unknown <zhizhu@zhizhu-devbox1.redmond.corp.microsoft.com>
2015-05-19 18:50:22 -0700

    FirstCommit

---
+++ MinidbgEngine/MinidbgEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minidbg.MinidbgEngine
+{
+    public class MinidbgEngine
+    {
+        private MinidbgProcess m_MinidbgProcess;
+
+        public MinidbgProcess CurrentProcess
+        {
+            get
+            {
+                return m_MinidbgProcess;
+            }
+
+            set
+            {
+                m_MinidbgProcess = value;
+            }
+        }
+    }
+}
---
+++ MinidbgShell/Shell.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minidbg.MinidbgShell
+{
+    using MinidbgEngine;
+    public class Shell
+    {
+        private MinidbgEngine m_MinidbgEngine;
+
+        public MinidbgEngine Debugger
+        {
+            get
+            {
+                return m_MinidbgEngine;
+            }
+
+            set
+            {
+                m_MinidbgEngine = value;
+            }
+        }
+
+
+        public void ShellInit()
+        {
+            Debugger = new MinidbgEngine();
+        }
+
+        class Commands
+        {
+            public static void Run(string[] args)
+            {
+                    
+            }
+        }
+    }
+
+}
